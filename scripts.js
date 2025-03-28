d29fb15cd219248b70b29b20d91bbbb1dc0173ab
Diogorss <diogorossis@hotmail.com>
2025-03-28 20:17:14 -0300

    Adicionando projetos

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
 const myObserver = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) { // Se o elemento está visível na tela
-            entry.target.classList.add('show'); // Adiciona a classe que ativa a animação
-            myObserver.unobserve(entry.target); // Remove a observação após exibir
-        }
-    });
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // Se o elemento está visível na tela
+      entry.target.classList.add("show"); // Adiciona a classe que ativa a animação
+      myObserver.unobserve(entry.target); // Remove a observação após exibir
+    }
+  });
 });
 
 // Seleciona todos os elementos com a classe "hidden"
-const elements = document.querySelectorAll('.hidden');
+const elements = document.querySelectorAll(".hidden");
 
 elements.forEach((element) => myObserver.observe(element)); // Observa cada elemento
